ede94a6607eed57d77dabb2cc2f36468201a0afe
Vinicius <53925893+ViniGusmao-png@users.noreply.github.com>
2026-06-04 12:21:57 -0300

    feat:criação de paginas do menu

---
+++ app/equip/page.tsx
@@ -0,0 +1,8 @@
+export default function Equip(){
+    return(
+       <div className="bg-black min-h-screen flex relative items-center justify-center">
+             <div className="border-5 rounded-md border-[#D7D7D7] bg-linear-to-br from-[#0054AC] to-[#010030] w-[65%] h-[90vh] "> {/*camada azul*/}
+             </div>
+           </div>
+    )
+}
--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,18 +1,23 @@
+import { useRouter } from 'next/navigation'
+
 export default function Menu() {
+    const router = useRouter()
     return(
         <div className="py-5 flex flex-col items-center justify-between border-5 rounded-md border-[#D7D7D7] bg-linear-to-br from-[#0054AC] to-[#010030]  w-[200] h-[60vh] relative">
             <div className="flex flex-col gap-3">
-                <h2 className="text-3xl text-white">Item</h2>
-                <h2 className="text-3xl text-white">Magic</h2>
-                <h2 className="text-3xl text-white">Materia</h2>
-                <h2 className="text-3xl text-white">Equip</h2>
-                <h2 className="text-3xl text-white">Status</h2>
-                <h2 className="text-3xl text-white">Order</h2>
-                <h2 className="text-3xl text-white">Limit</h2>
+                <button className="text-3xl text-white" onClick={() => router.push("/item")}>Item</button>
+                <button className="text-3xl text-white" onClick={() => router.push("/magic")}>Magic</button>
+                <button className="text-3xl text-white" onClick={() => router.push("/materia")}>Materia</button>
+                <button className="text-3xl text-white" onClick={() => router.push("/equip")}>Equip</button>
+                <button className="text-3xl text-white" onClick={() => router.push("/status")}>Status</button>
+                <button className="text-3xl text-white" onClick={() => router.push("/order")}>Order</button>
+                <button className="text-3xl text-white" onClick={() => router.push("/limit")}>Limit</button>
+                <button className="text-3xl text-white" onClick={() => router.push("/phs")}>PHS</button>
+
             </div>
 
             <div>
-                <h2 className="text-3xl text-white">Save</h2>
+                <button className="text-3xl text-white">Save</button>
             </div>
 
         </div>
